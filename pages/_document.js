1c41c6a4dbaf87e8b3daa7af5b06c4bec7644f58
Meet Raut <meetraut3004@gmail.com>
2024-07-22 17:40:43 +0530

    favicon hojaa

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-          <link rel="icon" href="../public/favicon.ico" />
+          <link rel="icon" href="../favicon.ico" />
         </Head>
         <body>
           <Main />
